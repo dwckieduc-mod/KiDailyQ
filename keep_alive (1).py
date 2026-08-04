06408126825a1dc2158379539d4f689213502f1d
dwckie <dwckieduc@gmail.com>
2026-08-04 16:36:30 +0700

    Add files via upload

---
+++ keep_alive (1).py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot Discord đang chạy 24/7 trên Render!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run_web)
+    t.start()
