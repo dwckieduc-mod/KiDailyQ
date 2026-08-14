c43178d80cfd6bce9e6a4d1e0e84038f004238ac
dwckie <dwckieduc@gmail.com>
2026-08-14 18:09:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -48,8 +48,6 @@ async def main():
         await bot.load_extension("build.member")
         await bot.load_extension("build.rank")
         await bot.load_extension("build.admin")
-        await bot.load_extension("font.montserrat_bold")
-        await bot.load_extension("font.montserrat_medium")
         await bot.start(BOT_TOKEN)
 
 if __name__ == "__main__":
